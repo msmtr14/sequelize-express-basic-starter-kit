ef1fada89012e47c284c99a3250e3db73c278afa
msmtr14 <mohitsinghmlp@gmail.com>
2019-06-30 23:51:52 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const User = require('./models/user');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+
+app.use(errorController.get404);
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Mohit', email: 'mohit@gmail.com' });
+    }
+    return user;
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ routes/admin.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+
+module.exports = router;
